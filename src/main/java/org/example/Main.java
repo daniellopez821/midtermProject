6c67ba36279add001e8622ffc3ddb72541bf4652
daniellopez821 <daniellopmor19@gmail.com>
2025-10-05 18:04:10 -0400

    3rd commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -58,7 +58,6 @@ public class Main {
         for(int i = 0; i < list.size(); i++){
             if(list.get(i).getName().equals(item)){
                 list.remove(i);
-                System.out.println(item + " has been deleted");
             }
         }
 
